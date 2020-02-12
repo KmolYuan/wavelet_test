3e1bf025b77fd969e7acbb5d8f214caf73f5ef44
KmolYuan <pyslvs@gmail.com>
2020-02-12 22:00:35 +0800

    Fix boundary error.

--- normalization.m
+++ normalization.m
@@ -1,17 +1,17 @@
-function new_path = normalization(path)
-c = struct;
-for i = 1:length(path)
-    c(i).x = real(path(i));
-    c(i).y = imag(path(i));
+function new_path = normalization(old_path)
+path = struct;
+for i = 1:length(old_path)
+    path(i).x = real(old_path(i));
+    path(i).y = imag(old_path(i));
 end
-len(1:length(c) + 1) = 0;
+len(1:length(path) + 1) = 0;
 centre = struct('x', 0, 'y', 0);
-for i = 1:length(c)
+for i = 1:length(path)
     if i - 1 < 1
         continue
     end
-    c1 = c(i);
-    c2 = c(i - 1);
+    c1 = path(i);
+    c2 = path(i - 1);
     len(i) = hypot(c2.x - c1.x, c2.y - c1.y);
     centre.x = centre.x + (c2.x + c1.x) * len(i);
     centre.y = centre.y + (c2.y + c1.y) * len(i);
@@ -20,12 +20,12 @@ len(end) = sum(len);
 centre.x = centre.x / (2 * len(end));
 centre.y = centre.x / (2 * len(end));
 inertia = [0, 0, 0];  % xx, yy, xy
-for i = 1:length(c)
+for i = 1:length(path)
     if i - 1 < 1
         continue
     end
-    c1 = c(i);
-    c2 = c(i - 1);
+    c1 = path(i);
+    c2 = path(i - 1);
     inertia(1) = inertia(1) + len(i) * (...
         (c2.y - centre.y)^2 +...
         (c1.y - centre.y)^2 +...
@@ -47,14 +47,19 @@ if inertia(1) > inertia(2)
 elseif inertia(1) == inertia(2)
     alpha = 0;
 end
-bound = [min([c.x]), max([c.x]), min([c.y]), max([c.y])];
+bound = [min([path.x]), max([path.x]), min([path.y]), max([path.y])];
 w = bound(2) - bound(1);
-tm = [cos(alpha) / w, sin(alpha) / w, -bound(1) / w;
-      -sin(alpha) / w, cos(alpha) / w, -bound(3) / w;
+tm = [cos(alpha) / w, sin(alpha) / w, 0;
+      -sin(alpha) / w, cos(alpha) / w, 0;
       0, 0, 1];
-new_path(1:length(c)) = 0;
-for i = 1:length(c)
-    t = tm * [c(i).x; c(i).y; 1];
-    new_path(i) = t(1, 1) + t(2, 1) * 1j;
+for i = 1:length(path)
+    t = tm * [path(i).x; path(i).y; 1];
+    path(i).x = t(1, 1);
+    path(i).y = t(2, 1);
+end
+bound = [min([path.x]), max([path.x]), min([path.y]), max([path.y])];
+new_path(1:length(path)) = 0;
+for i = 1:length(path)
+    new_path(i) = path(i).x - bound(1) + (path(i).y - bound(3)) * 1j;
 end
 end
